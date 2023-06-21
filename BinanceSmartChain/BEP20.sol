1a64399b5af0132c00ab1d1b9224595589c7ee2a
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-20 23:32:00 -0400

    Update BEP20.sol

--- BinanceSmartChain/BEP20.sol
+++ BinanceSmartChain/BEP20.sol
@@ -1,4 +1,3 @@
-// An example of a simple BEP20 token implementation:
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -30,4 +29,23 @@ contract BEP20 {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
-       
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
