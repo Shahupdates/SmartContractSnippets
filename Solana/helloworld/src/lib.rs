f61fc3ac5e334c001cb0b9259cd5104954b311c3
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-20 23:39:53 -0400

    Create lib.rs

---
+++ Solana/helloworld/src/lib.rs
@@ -0,0 +1,27 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    if account.owner != program_id {
+        msg!("Account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    msg!("Hello, World!");
+
+    Ok(())
+}
