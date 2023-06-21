e2a6f8dc02f263c55d6ba90f0fa1631780fcec14
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-20 23:31:51 -0400

    Create BEP20.sol
    Original Commit

---
+++ BinanceSmartChain/BEP20.sol
@@ -0,0 +1,33 @@
+// An example of a simple BEP20 token implementation:
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SafeMath.sol";
+
+contract BEP20 {
+    using SafeMath for uint256;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+       
