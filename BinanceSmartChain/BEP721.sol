d7e9ae47ab075adc5e3a1e4e1ad17e6f24502322
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-20 23:32:17 -0400

    Create BEP721.sol

---
+++ BinanceSmartChain/BEP721.sol
@@ -0,0 +1,20 @@
+// A simple BEP721 non-fungible token (NFT) implementation:
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721.sol";
+
+contract SimpleBEP721 is ERC721 {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("SimpleBEP721", "B721") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://my-json-server.typicode.com/demo/";
+    }
+
+    function mint(address to) public {
+        _mint(to, _tokenIdCounter);
+        _tokenIdCounter++;
+    }
+}
